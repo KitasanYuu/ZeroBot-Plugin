642c4c17be4cdf0465049c93f31a92668e65f0fd
KitasanYuu <107764844+KitasanYuu@users.noreply.github.com>
2022-07-10 18:46:03 +0800

    Update main.go

--- plugin/test1/main.go
+++ plugin/test1/main.go
@@ -4,12 +4,37 @@ import (
 	"fmt"
 	"test1/ID_Search"
 	"test1/handle"
-)
+	
+	"encoding/binary"
+	"fmt"
+	"image"
+	"image/color"
+	"os"
+	"path"
+	"regexp"
+	"sort"
+	"strconv"
+	"time"
 
-var USERID = "KitasanYuu"
+	ctrl "github.com/FloatTech/zbpctrl"
+	"github.com/FloatTech/zbputils/control"
+	"github.com/FloatTech/zbputils/ctxext"
+	"github.com/FloatTech/zbputils/file"
+	"github.com/FloatTech/zbputils/img"
+	"github.com/FloatTech/zbputils/img/text"
+	"github.com/FloatTech/zbputils/img/writer"
+	"github.com/FloatTech/zbputils/web"
+	"github.com/fogleman/gg"
+	log "github.com/sirupsen/logrus"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/message"
+)
+engine.OnRegex(`^>查id\s?(.{1,25})$`, getdb).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			keyword := ctx.State["regex_matched"].([]string)[1]}
 
 func main() {
-	Users := "https://robertsspaceindustries.com/citizens/" + USERID
+	Users := "https://robertsspaceindustries.com/citizens/" + keyword
 
 	h := handle.UserHandle{}
 	Search := ID_Search.NewSearch()
